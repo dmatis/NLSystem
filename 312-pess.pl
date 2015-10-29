f0174f8a626074a77af6312bb41d87e4f22ea5e1
dmatis <darren.matis@gmail.com>
2015-10-28 17:28:32 -0700

    added basic shell near the top

--- 312-pess.pl
+++ 312-pess.pl
@@ -2,7 +2,7 @@
 %% Author: Steve Wolfman
 %% Date: 2005/02/02
 %% Collaborators: based partially on work by David Lowe
-%% Sources: based partially on code from Amzi!, Inc. 
+%% Sources: based partially on code from Amzi!, Inc.
 %%
 %% Description:
 %% 312 Prolog Expert System Shell, based on Amzi's "Native" shell.
@@ -15,19 +15,19 @@
 
 
 % This file is broken into sections using banners like the
-% "Introduction" banner that begins this section. The key sections are: 
+% "Introduction" banner that begins this section. The key sections are:
 % - Interpreter loop: defines the entry point main/0 that gives a
 %   command prompt from which users can easily load files and solve
 %   problems.  This is left for you to write!
 % - Solving, asking, and proving: defines solve/0, which starts
 %   the expert system solving whatever the current top goal is.
 %   "ask" and "prove" are used to ask the user about the truth
-%   of goals or reason about them, respectively. 
+%   of goals or reason about them, respectively.
 % - Flattening attributes: Mostly a helper section for the "prove"
 %   predicate.  This defines flatten_attr/2, which takes a goal
-%   (attribute) and turns it into a large (ideally exhaustive) set 
+%   (attribute) and turns it into a large (ideally exhaustive) set
 %   of implied goals.
-% - Rule loading: load_rules/1 loads natural language rules from a 
+% - Rule loading: load_rules/1 loads natural language rules from a
 %   knowledge base (file)
 
 
@@ -48,14 +48,53 @@
 
 %%%%%%%%%%%%% TODO!! %%%%%%%%%%%%%%%%%%%%
 
+go:-
+	greeting,
+	repeat,
+	write('>'),
+	read(X),
+	do(X),
+	X == quit.
+
+greeting:-
+	write('This is the CPSC312 Prolog Expert System Shell.'),nl,
+	write('Based on Amzi\'s native Prolog shell.'),nl,
+	write('Type help. load. solve. or quit. at the prompt'),nl,
+	write('Note the period after each command!'),nl.
+
+do(load):-load_kb, !.
+
+do(consult):- solve, !.
+
+do(quit).
+
+do(X):-
+	write(X),
+	write('invalid command entered.'),nl,
+	fail.
+
+load_kb:-
+	write('Enter file name in single quotes, followed by a period'),nl,
+	write('(e.g. \'bird.kb\'.): '),nl,
+	read(F),
+	load_rules(F).
+
+help:-
+	write('Legal commands for this system are: '),nl,
+	write('help.'),nl,
+	write('load.'),nl,
+	write('solve.'),nl,
+	write('quit.').nl,
+	greeting.
+
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% Solving, asking, and proving                                 %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-% This section includes the solve, ask, and prove predicates which 
-% together form the heart of the expert system shell.  solve initiates 
-% an interaction with the shell (in which it tries to solve the user's 
+% This section includes the solve, ask, and prove predicates which
+% together form the heart of the expert system shell.  solve initiates
+% an interaction with the shell (in which it tries to solve the user's
 % top goal), 'ask' asks the user whether a given assertion is true or
 % false, and 'prove' recursively proves assertions (essentially using
 % Prolog's own depth-first, backtracking search solution scheme).
@@ -116,7 +155,7 @@ prove([Goal|Rest], Hist) :- prove_one(Goal, [Goal|Hist]),
                             prove(Rest, Hist).
 
 
-% prove_one(X) is true if X is known directly, implied by the DB, or 
+% prove_one(X) is true if X is known directly, implied by the DB, or
 % supplied by the user.  To avoid pestering the user, prove_one(X) fails
 % if X is already known to be false (or the database implies that it is
 % false).
@@ -124,12 +163,12 @@ prove_one(true, _) :- !.                   % Facts give a body of 'true'.
 prove_one(X, _) :- known(X), !.            % Note: would be found w/implied.
 prove_one(X, _) :- known(not(X)), !, fail. % Note: would be found w/implied.
 prove_one(X, _) :- implied(X), !.          % Known or implied: proven
-prove_one(X, _) :- implied(not(X)), 
+prove_one(X, _) :- implied(not(X)),
         !, fail.                           % Known/implied NOT: disproven
 prove_one(X, Hist) :-                      % Where the action is.  There's
-        rule(X, Body),                     % some goal that can prove 
+        rule(X, Body),                     % some goal that can prove
         prove(Body, Hist).                 % this; so, give it a shot.
-prove_one(X, Hist) :- \+ rule(X, _),       % If there's no goal to prove 
+prove_one(X, Hist) :- \+ rule(X, _),       % If there's no goal to prove
                       ask(X, Hist), !,     % this, ask the user and
                       known(X).            % trust the answer.
 
@@ -141,7 +180,7 @@ prove_one(X, Hist) :- \+ rule(X, _),       % If there's no goal to prove
 % NOT do this.
 
 % implied(X) is true if X is known directly or implied by the database.
-% Note: implied assumes that "not", if it appears, appears only as the 
+% Note: implied assumes that "not", if it appears, appears only as the
 % outermost predicate of its argument.
 implied(not(X)) :- !, flatten_attr(X, Y), prove_not_some(Y).
 implied(X)      :-    flatten_attr(X, Y), prove_all(Y).
@@ -154,7 +193,7 @@ implied(X)      :-    flatten_attr(X, Y), prove_all(Y).
 prove_all([]).
 prove_all([Assert|Asserts]) :- known(Assert), prove_all(Asserts).
 
-% prove_not_some(Asserts) is true if any element of Asserts is known to 
+% prove_not_some(Asserts) is true if any element of Asserts is known to
 % be false.  The cut is for efficiency (and to avoid redundant solns).
 prove_not_some([Assert|_]) :- known(not(Assert)), !.
 prove_not_some([_|Asserts]) :- prove_not_some(Asserts).
@@ -164,7 +203,7 @@ prove_not_some([_|Asserts]) :- prove_not_some(Asserts).
 %%%%%%%%%%%%%%% ask and related %%%%%%%%%%%%%%%%%%%
 
 % Ask the user whether the fact X is true.
-% Uses Hist to tell the user (or, more likely, the developer) how 
+% Uses Hist to tell the user (or, more likely, the developer) how
 % the system got to its current state.
 ask(X, Hist) :- plain_gloss([X], Y), !,       % Accept only the first gloss.
                 write('Would you say that '), % Prompt for..
@@ -175,32 +214,32 @@ ask(X, Hist) :- plain_gloss([X], Y), !,       % Accept only the first gloss.
                                               % reflect the user's answer
 
 % Update the knowledge base to reflect a user's assertion.
-update(yes, X) :- !,          
+update(yes, X) :- !,
       asserta(known(X)),      % Add X and all its implied knowledge
-      flatten_attr(X, Y),     % to the knowledge database. 
+      flatten_attr(X, Y),     % to the knowledge database.
       assert_all(Y).          % (Note: tough to get this back out. Could
                               % use a unique ID for later reference.)
-update(_, X)   :-         
+update(_, X)   :-
       retract(known(X)),      % Pull out known(X).
       !, fail.                % Then, fail to backtrack to next rule.
                               % Note: does NOT retract everything based on X.
-                              % and probably should not, either 
+                              % and probably should not, either
                               % (De Morgan's!)
-update(_, X)   :- 
+update(_, X)   :-
       asserta(known(not(X))). % Put in known(not(X)).  As above,
                               % does not put in everything flattened
-                              % out of not(X) b/c these facts are not 
+                              % out of not(X) b/c these facts are not
                               % necessarily true (not implied!)
 
 % Assert a list of knowledge using the known/1 predicate.
-assert_all([]).                     
-assert_all([Assert|Asserts]) :-     
-          asserta(known(Assert)),   
-          assert_all(Asserts).      
+assert_all([]).
+assert_all([Assert|Asserts]) :-
+          asserta(known(Assert)),
+          assert_all(Asserts).
 
 % Get the user's response to a question, handling "why" questions.
 get_user(X,Hist) :-
-        repeat,                  
+        repeat,
         write('> '),             % Ask for the user's answer
         read(Y),
         process_ans(Y,Hist,X),   % Either read answer or field question
@@ -232,17 +271,17 @@ write_list(N,[H|T]) :-
 % if it has long, sharp claws, then it surely has long claws, and has
 % sharp claws, and has claws.
 
-% flatten_attr calculates the list of smaller attributes 
+% flatten_attr calculates the list of smaller attributes
 % corresponding to one larger attribute (all those attributes
 % that are NOT conjunctive that are implied by the original
-% attribute; that is, for each attr(Type, Value, SubAttributes) 
+% attribute; that is, for each attr(Type, Value, SubAttributes)
 % in the flattened attribute list, SubAttributes is either empty or
 % has one element.)
 % This is useful for a few reasons:
 % - each of these "flattened" attributes is implied by the original
 % - conversely, if any of these flattened attributes is false, the
 %   original is also false
-% - finally, if all of these flattened attributes can be proven, 
+% - finally, if all of these flattened attributes can be proven,
 %   so too can the original.
 % In fact, that last requirement is too strong in a sense. Only the
 % "deepest" attributes need to be proven; the shallower ones are implied
@@ -265,7 +304,7 @@ write_list(N,[H|T]) :-
 % "it eats insects" does NOT imply that "it slowly eats insects".
 %
 % Some notes about fixing the flattened true -> original true assertion:
-% - There's really no need to fix the fact that the statement is too 
+% - There's really no need to fix the fact that the statement is too
 %   strong.  As long as the flattened attributes are proven IN REVERSE
 %   ORDER, each implied attribute will already have been proven (and
 %   so be in the knowledge base) by the time the proof process reaches
@@ -274,7 +313,7 @@ write_list(N,[H|T]) :-
 %   flatten_attr.  Instead, I would change the way the parser represents
 %   rules and goals.  For example, "it slowly eats insects" might become
 %   something like:
-%     attr(does, eats, [attr(is_how, slowly, [attr(is_a, insects, [])])]) 
+%     attr(does, eats, [attr(is_how, slowly, [attr(is_a, insects, [])])])
 %   rather than:
 %     attr(does, eats, [attr(is_how, slowly,  []),
 %                       attr(is_a,   insects, []) ])
@@ -291,17 +330,17 @@ flatten_attr(X, Y) :- flatten_attr_dl(X, Y\[]).
 % - for an attribute attr(Type, Value, SubAttributes),
 %   include the bare attribute (i.e., attr(Type, Value, [])) plus
 %   the attribute with paired with each flattened sub attribute
-%   (i.e., attr(Type, Value, FlattenedSubAttrsListElt)) 
+%   (i.e., attr(Type, Value, FlattenedSubAttrsListElt))
 flatten_attr_dl([], Soln\Soln) :- !.         % Empty list produces nothing.
 
-flatten_attr_dl([X|Xs], Soln\Rest) :- !,     % A list gets 
+flatten_attr_dl([X|Xs], Soln\Rest) :- !,     % A list gets
         flatten_attr_dl(X, Soln\Mid),        % flattened inside and
         flatten_attr_dl(Xs, Mid\Rest).       % joined together.
 
-flatten_attr_dl(attr(Type, Value, Subs), 
-                [attr(Type, Value, [])|RestSoln]\Rest) :- !, 
-        flatten_attr(Subs, FlatSubs),  % Note the base attribute was added. 
-        prepend_attr(attr(Type, Value, []), 
+flatten_attr_dl(attr(Type, Value, Subs),
+                [attr(Type, Value, [])|RestSoln]\Rest) :- !,
+        flatten_attr(Subs, FlatSubs),  % Note the base attribute was added.
+        prepend_attr(attr(Type, Value, []),
                      FlatSubs,         % Flatten the sub attributes, and
                      RestSoln\Rest).   % prepend the base attribute to each.
 
@@ -310,12 +349,12 @@ flatten_attr_dl(X, [X|Rest]\Rest).   % Base case and catchall for
                                      % the list of itself.
 
 % prepend_attr transforms the input list into a list of attributes.
-% prepend_attr(attr(Type, Val, _), SubAttrs, Attrs\Attrr) is true 
+% prepend_attr(attr(Type, Val, _), SubAttrs, Attrs\Attrr) is true
 % if Attrss\Attrr is SubAttrs except each element SubAttr of SubAttrs
 % corresponds to attr(Type, Val, [SubAttr]) in Attrs\Attrr.
 prepend_attr(_, [], Soln\Soln).
-prepend_attr(attr(Type, Val, _), 
-             [Sub|Subs], 
+prepend_attr(attr(Type, Val, _),
+             [Sub|Subs],
              [attr(Type, Val, [Sub])|RestSoln]\Rest) :-
         prepend_attr(attr(Type, Val, _), Subs, RestSoln\Rest).
 
@@ -370,8 +409,8 @@ clear_db :-
         assertz(rule(top_goal(X), [attr(is_a, X, [])])).
 
 % Gloss a rule for debugging output.
-bug(X) :- write('Understood: '), 
-        plain_gloss(X, Text), 
+bug(X) :- write('Understood: '),
+        plain_gloss(X, Text),
         write_sentence(Text), nl.
 bug(X) :- write(X).
 
